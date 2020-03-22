69157977941f90ed7e97ce3f76379c45119bc2e3
John Westbury <62035265+westbuj@users.noreply.github.com>
2020-03-22 06:34:13 -0700

    Update SolarLight_ATTINY.ino

--- SolarLight_ATTINY.ino
+++ SolarLight_ATTINY.ino
@@ -1,17 +1,12 @@
 /*
   Solar Powered Lighted Sign
-
   This sketch uses an ATTINY85 to monitor a 6v solar cell and 4.2v battery.
-
   When the sun goes down (solar cell voltage is low), a PWM signal is sent to a
   MOSFET. The MOSFET is connected to an LED. When battery voltage drops below about 
   3.2 v, the signal to the MOSFET is stopped and the LED is turned off.
-
   When the LED is OFF, the ATTINY is put into sleep-power down to save battery power.
-
   Because the solar cell is 6v, a voltage divider is required to bring the voltage into
   range for the analog input.
-
   For monitoring, SendOnlySoftwareSerial is used to send a 9600 baud output stream.
   Connect this to an FTDI USB interface or another Arduino to monitor the stream.
   
@@ -19,10 +14,10 @@
   
   Circuit:
   - ATTINY85
-  - Connect a solar cell to a charge controller.
-  - Connect the charge controller to the battery.
+  - Connect a 6v solar cell to a charge controller.
+  - Connect the charge controller output to the battery.
   - connect a 5v booster/regulator to the battery with output to the ATTINY.
-      A constant supply voltage is required as a refernece for the analog to digital conversion.
+      A constant supply voltage is required as a referenece for the analog to digital conversion.
       The ATTINY could be connected to the 4.2v cell, but the reference would change as the battery charge changes.
   - use a voltage divider on the + and - of the solar cell (or the charge controller input).
       solar + 10k ohm--------ATTINY (A3)---------2k ohm solar - 
@@ -31,23 +26,20 @@
   - Connect the LED + to the battery +
   - Connect ATTINY A1 to the battery + with a 1k ohm current limiting resistor
       ATTTINY A1 ---->  1k ohm   -------> Battery +
-      This allows the ATTINTY to be programmed without problems
+      This allows the ATTINTY to be programmed while in circuit.
   - connect all the grounds together
       battery, solar cell, and ATTINY
-      don't connect the LED - to ground. This goes through the N channel MOSFET.
+      don't connect the LED - to ground. This connection goes through the N channel MOSFET.
   
-     
 
+The ATTINY85 does not have Serial output. We are tricking the IDE by declaring a 
+SendOnlySoftwareSerial called Serial.
+
+     
   created 3 Mar 2020
   by John Westbury
-
-
-
 */
-// todo : increase sleep cycles  -- shoot for 8 mins (about 64 cycles)
-//        delay after sunset before going on -- and/or lower sun threshold
-//        put a maximum time on light on -- like 3 hours
-//        determin battery shut off -- about 3.2 v
+
 
 
 #include <SendOnlySoftwareSerial.h>
@@ -58,39 +50,54 @@
 #define Solar_Pin A3
 #define Battery_Pin A1
 #define MOSFET_Pin 0
-#define SLEEP_CYCLES 1   // Each Cycle is eight seconds
+#define SLEEP_CYCLES 72   // Each Cycle is eight seconds
 
 SendOnlySoftwareSerial Serial (4);
 
 void setup() {
  
-  Serial.begin(9600);
+  Serial.begin(9600); 
   Serial.println("\n\n\nRun----\n");
   
 }
 
 
+bool allowLED = true;
+
 void loop() {
-
-  delay(200);
-  int sunValue = analogRead(Solar_Pin);
-  int batValue = analogRead(Battery_Pin);
-
+    Serial.println("LOOP Begin----");
+    long sunValue = analogRead(Solar_Pin);
+    long batValue = analogRead(Battery_Pin);
     
+  for (int j=0;j<20;j++){
+    delay(20);  
+    sunValue = (analogRead(Solar_Pin) + sunValue + sunValue)/3;
+    batValue = (analogRead(Battery_Pin) + batValue + batValue)/3;
+   
+  }
+
+  if (sunValue >= 500) allowLED = true;  
+  if (batValue <  700) allowLED = false;
+  
   Serial.print("S V = ");
   Serial.println(sunValue);
-  Serial.print("B V = ");
+  Serial.print("B Avg = ");
   Serial.println(batValue);
+  Serial.print("ALLOW = ");
+  Serial.println(allowLED);
 
-  if (sunValue<500 && batValue > 600)
+  
+  if (sunValue < 400 && batValue > 700 && allowLED)
   {
-    Serial.println("ON");
+    Serial.println("ON - delay");
     analogWrite(MOSFET_Pin,32);
     delay(5000);
   }
-  if (sunValue>500  || batValue < 600)
+   
+  if (sunValue>400  || batValue < 700)
   {
-    Serial.println("OFF");
+    Serial.println("OFF - SLEEP");
+    
     analogWrite(MOSFET_Pin,0);
     for (int j=0;j<SLEEP_CYCLES;j++)
     { shutDown_with_WD (0b100001);  // 8 seconds
@@ -99,9 +106,9 @@ void loop() {
   }
   
   
-   
-  Serial.println("back");
+  delay(5000);
 }
+
 
 //This function is called when the watchdog timer expires. 
 
@@ -111,7 +118,7 @@ ISR(WDT_vect) {
     
 }
 
-//This function puts the ATTINY in sleep mode for 8 seconds.
+//This function puts the ATTINY in sleep mode.
 //To save power, the ADCs are explicitly shutdown.
  
 void shutDown_with_WD(const byte time_len)
@@ -121,11 +128,11 @@ void shutDown_with_WD(const byte time_len)
 
   wdt_reset();
    
-  MCUSR = 0;                          // reset various flags
-  WDTCR |= 0b00011000;               // see docs, set WDCE, WDE
-  WDTCR =  0b01000110 | time_len;    // set WDIE, and appropriate delay
+  MCUSR = 0;                          // reset flags
+  WDTCR |= 0b00011000;               // set WDCE, WDE
+  WDTCR =  0b01000110 | time_len;    // set WDIE and delay
  
-  ADCSRA &= ~_BV(ADEN);
+  ADCSRA &= ~_BV(ADEN);              //Stop the adc
 
   set_sleep_mode (SLEEP_MODE_PWR_DOWN);
   sleep_bod_disable();
